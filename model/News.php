b0cc22a36ebc9864da9cb6a789553e72edb52d61
maxmattone <mattone.m@gmail.com>
2014-04-23 11:22:48 +0200

    debug youtube

--- model/News.php
+++ model/News.php
@@ -93,9 +93,9 @@ class News extends ActiveRecord\Model {
     }
 
     public function youtube($address) {
-        return '<iframe title="YouTube video'. 
-            'player" class="youtube-player" type="text/html"'. 
-            'width="400" height="300" src="' . $address . '"'.
+        return '<iframe title="YouTube video '. 
+            'player" class="youtube-player" type="text/html" '. 
+            'width="400" height="300" src="' . $address . '" '.
             'frameborder="0" allowFullScreen></iframe>';
     }
 

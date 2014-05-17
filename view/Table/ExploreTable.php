584441f3e51c42e353a0679103d0cb7f1a66b9b1
maxmattone <mattone.m@gmail.com>
2014-05-17 12:00:16 +0200

    add link to edit mode

--- view/Table/ExploreTable.php
+++ view/Table/ExploreTable.php
@@ -2,6 +2,11 @@
 class ExploreTable extends View{
     public function render($models) {
         $content = "";
+        $content .= ButtonWidget::renderWidget(
+                    Controller::urlFor('Table', 'editTable', array('table' => get_class($models[0]))),
+                    'Edit',
+                    'primary'
+                );
         $content .= IndexTableWidget::renderWidget(
                 $models
                 );
